76d168ace572be9158d451a3098c13e79492e4f3
imoddinov <imoddinov@gmail.com>
2024-11-07 18:27:53 +0500

    task 7

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from typing import Optional
+from fastapi.responses import HTMLResponse
+import uvicorn
 
 
-# Press the green button in the gutter to run the script.
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello():
+    html_content = "<h2>Hello World!</h2>"
+    return HTMLResponse(content=html_content)
+
+@app.get('/info/server')
+def get_uvicorn_version():
+    version = uvicorn.__version__
+    html_content = f"<h2>uvicorn_version: {version}!</h2>"
+    return HTMLResponse(content=html_content)
+
+
+def dto():
+    print("")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    dto()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
